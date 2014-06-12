5d04ea11ba4b07835d1ac9862e6187fdc71fd214
Denis Ivanov <visible@jumph4x.net>
2014-06-11 23:24:44 -0700

    More layout tweaks

--- app/models/spree/image_decorator.rb
+++ app/models/spree/image_decorator.rb
@@ -1,7 +1,7 @@
 module Spree
   Image.class_eval do
     has_attached_file :attachment,
-                      styles: { mini: '48x48>', small: '192x192>', product: '384x384>', large: '777x777>' },
+                      styles: { mini: '48x48>', small: '192x192>', product: '465x465>', large: '777x777>' },
                       default_style: :product,
                       url: '/spree/products/:id/:style/:basename.:extension',
                       path: ':rails_root/public/spree/products/:id/:style/:basename.:extension',
