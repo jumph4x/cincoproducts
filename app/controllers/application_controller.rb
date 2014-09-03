c5f26d4a2597645502171b29f973ae2d0403a1d2
Denis Ivanov <visible@jumph4x.net>
2014-09-02 17:27:26 -0700

    Redirect unless WWW in the host

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,11 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_filter :redirect_to_www
+
+  def redirect_to_www
+    return true if !Rails.env.production? || request.host.match(/www\./)
+    redirect_to "http://www.cincoproducts.com"
+  end
 end
